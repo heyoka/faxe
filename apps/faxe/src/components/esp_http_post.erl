8f4e6acffa9dfd81db69a43103ace29aef229e29
heyoka <erdverbindung@gmx.at>
2019-10-07 09:32:01 +0200

    --

--- apps/faxe/src/components/esp_http_post.erl
+++ apps/faxe/src/components/esp_http_post.erl
@@ -31,6 +31,7 @@ options() ->
 
 init(_NodeId, _Inputs, #{host := Host0, port := Port, path := Path, key := Key}) ->
    Host = binary_to_list(Host0)++":"++integer_to_list(Port),
+   lager:notice("host part is: ~p" ,[Host]),
    {ok, C} = fusco:start(Host, []),
    {ok, all, #state{host = Host, port = Port, path = Path, key = Key, client = C}}.
 
@@ -44,11 +45,18 @@ process(_In, B = #data_batch{}, State = #state{}) ->
 send(Item, #state{host = Host, port = Port, path = Path, key = Key, client = Client}) ->
    M = flowdata:to_mapstruct(Item),
    #{<<"ts">> := Ts, <<"df">> := Df, <<"vs">> := Vs, <<"data">> := Data, <<"id">> := Id} = M,
-   Stmt = <<"INSERT INTO tstest (ts, id, df, vs, fields) VALUES(?,?,?,?,?)">>,
+   Stmt = <<"INSERT INTO robotplc (ts, id, df, vs, data_obj) VALUES(?,?,?,?,?)">>,
    BulkArgs = [Ts, Id, Df, Vs, Data],
 %%   lager:notice("mapstruct: ~p", [M]),
-   Query = #{<<"stmt">> => Stmt, <<"bulk_args">> => BulkArgs},
+   Query = #{Key => Stmt, <<"args">> => BulkArgs},
+%%   Url = Host ++ "/" ++ Path,
+%%   Response =
+%%   httpc:request(post,
+%%      {Url, [],
+%%         "application/json",
+%%         jiffy:encode(Query)
+%%      }, [], []),
    Response = fusco:request(Client, Path, "POST", [], jiffy:encode(Query), 10000),
-   lager:notice("Query: ~p :: Response :: ~p",[jiffy:encode(Query), Response]),
+   lager:notice("Query: ~s :: Response :: ~p",[jiffy:encode(Query), Response]),
 %%   io:format(Host, "~s~n", [binary_to_list(flowdata:to_json(Item))]).
 ok.
