a8f2f10f58f372db2b960d35252ea7c10edda6f2
heyoka <erdverbindung@gmx.at>
2020-01-30 20:35:03 +0100

    nodeparams for convenience

--- apps/faxe/src/lib/faxe_node_params.erl
+++ apps/faxe/src/lib/faxe_node_params.erl
@@ -13,10 +13,18 @@
 params(<<"combine">>) -> [{1, 2}];
 params(<<"join">>) -> {all, new_port, 1}.
 
-options(<<"deadman">>) -> [{1, <<"interval">>, duration},{2, <<"threshold">>, int}];
+%% (component name binary) -> [{param number|all, param name, param_type}]
+options(<<"deadman">>) -> [{1, <<"timeout">>, duration},{2, <<"threshold">>, int}];
 options(<<"shift">>) -> [{1, <<"offset">>, duration}];
-%%options(<<"modbus">>) -> [{1, <<"function">>, binary}];
 options(<<"where">>) -> [{1, <<"lambda">>, lambda}];
+options(<<"eval">>) -> [{all, <<"lambdas">>, lambda_list}];
+options(<<"keep">>) -> [{all, <<"fields">>, binary_list}];
+options(<<"delete">>) -> [{all, <<"fields">>, binary_list}];
+options(<<"file">>) -> [{1, <<"file">>, string}];
+options(<<"sample">>) -> [{1, <<"rate">>, string}];
+options(<<"state_count">>) -> [{1, <<"lambda">>, lambda}];
+options(<<"state_duration">>) -> [{1, <<"lambda">>, lambda}];
+options(<<"case">>) -> [{all, <<"lambdas">>, lambda_list}];
 options(_) -> undefined.
 
 %% helper functions
