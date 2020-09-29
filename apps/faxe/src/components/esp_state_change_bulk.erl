4b457057a92b24fa3e25bda2e9c4ef4027530b3e
heyoka <erdverbindung@gmx.at>
2020-09-29 17:11:44 +0200

    state_change_bulk exclude option

--- apps/faxe/src/components/esp_state_change_bulk.erl
+++ apps/faxe/src/components/esp_state_change_bulk.erl
@@ -2,7 +2,9 @@
 %% Ⓒ 2019 heyoka
 %% @doc
 %% Computes the duration of a given state for all fields in a given object.
-%% The state is defined via a lambda expression pattern.
+%% It is assumed that 'field' is an object containing only plain fields (no other objects or lists).
+%% The state is defined via a lambda expression pattern (example: '$field =< 2 orelse $field == 222').
+%% Note that the lambda pattern must be valid erlang code and only all $field placeholders will be replace by the current fieldname.
 %% Timestamps for the duration are taken from the incoming data-point.
 %%
 %% For each consecutive point for which the lambda expression evaluates as true,
@@ -41,12 +43,14 @@
    unit,
    state_changes = [],
    prefix,
-   field
+   field,
+   excluded = []
 }).
 
 options() -> [
    {lambda_pattern, string},
    {field, string},
+   {exclude_fields, string_list, []},
    {enter_as, binary, <<"state_entered">>},
    {leave_as, binary, <<"state_left">>},
    {enter, is_set, undefined},
@@ -63,11 +67,13 @@ check_options() ->
    ].
 
 init(_NodeId, _Ins, #{lambda_pattern := Lambda_Pattern, enter_as := EnteredAs, leave_as := LeftAs, enter := EmitEntered,
-   leave := EmitLeft, enter_keep := KeepFieldsEntered, leave_keep := KeepFieldsLeft, prefix := Prefix, field := Field}) ->
+   leave := EmitLeft, enter_keep := KeepFieldsEntered, leave_keep := KeepFieldsLeft, prefix := Prefix, field := Field,
+   exclude_fields := Excluded}) ->
 
    {ok, all,
       #state{
          field = Field,
+         excluded = Excluded,
          lambda_pattern = Lambda_Pattern,
          state_lambdas = [],
          emit_entered = EmitEntered,
@@ -83,12 +89,11 @@ process(_In, #data_batch{points = _Points} = _Batch, _State = #state{state_lambd
    {error, not_implemented};
 process(_Inport, #data_point{} = Point, State = #state{field = Field}) ->
    FieldMap = flowdata:field(Point, Field),
-%%   lager:notice("FieldMap:~p",[FieldMap]),
    StateTrackers = get_states(FieldMap, State),
    F = fun({FieldName, StateChange}, UpdatedStateChanges) ->
       case state_change:process(StateChange, Point) of
          {ok, NewStateChange} ->
-            handle(state_change:get_state(NewStateChange), NewStateChange, State),
+            handle(state_change:get_state(NewStateChange), FieldName, NewStateChange, State),
             [{FieldName, NewStateChange} | UpdatedStateChanges];
          {error, Error} ->
             lager:error("Error evaluating lambda: ~p",[Error]),
@@ -98,26 +103,28 @@ process(_Inport, #data_point{} = Point, State = #state{field = Field}) ->
    NewStates = lists:foldl(F, [], StateTrackers),
    {ok, State#state{state_changes = NewStates}}.
 
-handle(entered, StateState, State=#state{emit_entered = true, entered_as = As, entered_keep = Keep}) ->
+handle(entered, FieldName, StateState, State=#state{emit_entered = true, entered_as = As, entered_keep = Keep}) ->
    P = state_change:get_last_point(StateState),
-   emit_point_data(P, Keep, [As], [1], State);
-handle(left, StateState, State=#state{emit_left = true, left_as = As, left_keep = Keep}) ->
+   emit_point_data(P, Keep, [As, <<"field">>], [1, FieldName], State);
+handle(left, FieldName, StateState, State=#state{emit_left = true, left_as = As, left_keep = Keep}) ->
    P = state_change:get_last_point(StateState),
    AddFNames = [
       As,
       <<"state_start_ts">>,
       <<"state_end_ts">>,
       <<"state_duration">>,
-      <<"state_count">>],
+      <<"state_count">>,
+      <<"field">>],
    AddFields = [
       1,
       state_change:get_last_enter_time(StateState),
       P#data_point.ts,
       state_change:get_last_duration(StateState),
-      state_change:get_last_count(StateState)],
+      state_change:get_last_count(StateState),
+      FieldName],
 
    emit_point_data(P, Keep, AddFNames, AddFields, State);
-handle(_, _StateState, State=#state{}) ->
+handle(_, _F, _StateState, State=#state{}) ->
    {ok, State}.
 
 emit_point_data(P, Keep, AddFieldNames, AddFieldVals, _State = #state{prefix = Prefix}) ->
@@ -127,27 +134,37 @@ emit_point_data(P, Keep, AddFieldNames, AddFieldVals, _State = #state{prefix = P
    dataflow:emit(flowdata:set_fields(NewPoint, FieldNames++Keep, AddFieldVals++Fields)).
 
 
-get_states(FieldMap, #state{lambda_pattern = Pattern, state_changes = States}) ->
+get_states(FieldMap, #state{lambda_pattern = Pattern, state_changes = States, field = ParentField, excluded = Ex}) ->
    Fields = maps:keys(FieldMap),
-   lager:notice("fields: ~p",[Fields]),
-   lists:map(
+   lists:foldl(
       fun
-         (FieldName) ->
-         case proplists:get_value(FieldName, States) of
-            undefined ->
-               Fun = build_fun(Pattern, FieldName),
-               {FieldName, state_change:new(Fun)};
-            Fun ->
-               {FieldName, Fun}
-         end
+         (FieldName0, CurrentStates) ->
+            FieldName = <<ParentField/binary, ".", FieldName0/binary>>,
+            case lists:member(FieldName, Ex) of
+               true ->
+                  CurrentStates;
+               false ->
+                  case proplists:get_value(FieldName, States) of
+                     undefined ->
+                        Fun = build_fun(Pattern, FieldName),
+                        [{FieldName, state_change:new(Fun)}|CurrentStates];
+                     Fun ->
+                        CurrentStates
+                  end
+            end
       end,
+      States,
       Fields).
 
-build_fun(PatternString, FieldName ) when is_binary(PatternString) ->
-   FunString = binary:replace(PatternString, ?PLACE_HOLDER, string:titlecase(FieldName)),
-   lager:notice("Fun: ~p",[FunString]),
-   VarBindings = [{string:titlecase(Var), list_to_binary(string:lowercase(Var))} || Var <- [binary_to_list(FieldName)]],
-   {Vars, Bindings} = lists:unzip(VarBindings),
+build_fun(PatternString, FieldName) when is_binary(PatternString) ->
+   Name = clean_param_name(FieldName),
+   FunString = binary:replace(PatternString, ?PLACE_HOLDER, string:titlecase(Name), [global]),
+   Vars = [string:titlecase(binary_to_list(Name))],
+   Bindings = [FieldName],
    faxe_dfs:make_lambda_fun(binary_to_list(FunString), Vars, Bindings).
+
+clean_param_name(Name) when is_binary(Name) ->
+   S0 = re:replace(Name, "[^a-zA-Z0-9_.]", <<"_">>, [{return, binary}]),
+   binary:replace(S0, [<<".">>,<<"[">>,<<"]">>], <<"_">>, [global]).
 
 
