fb2701d56963a96445e4cbb8242d46b492b102c1
heyoka <erdverbindung@gmx.at>
2021-04-13 14:47:34 +0200

    v0.14.0

--- apps/faxe/src/components/esp_group_by.erl
+++ apps/faxe/src/components/esp_group_by.erl
@@ -63,8 +63,7 @@ ensure_route(Val, Groups, _NId) when map_size(Groups) == 0 ->
 ensure_route(Val, Groups, _NId) when is_map_key(Val, Groups) ->
    Groups;
 ensure_route(Val, Groups, NodeId) ->
-   {T, {ok, NewPort}} = timer:tc(df_graph, start_subgraph, [NodeId]),
-   lager:alert("time to start subgraph: ~p",[T]),
+   {ok, NewPort} = df_graph:start_subgraph(NodeId),
    Groups#{Val => NewPort}.
 
 %% not used
