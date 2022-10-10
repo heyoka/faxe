4cabb3a7d52b702f1e31ec659e7b6cbc03ab934a
heyoka <erdverbindung@gmx.at>
2022-10-10 14:55:38 +0200

    sup

--- apps/faxe/src/faxe_sup.erl
+++ apps/faxe/src/faxe_sup.erl
@@ -67,10 +67,10 @@ init([]) ->
         {s7pool_sup,
             {s7pool_sup, start_link, []},
             permanent, infinity, supervisor, [s7pool_sup]}
-        ,
-        {s7pool_sup_native,
-            {s7pool_sup_native, start_link, []},
-            permanent, infinity, supervisor, [s7pool_sup_native]}
+%%        ,
+%%        {s7pool_sup_native,
+%%            {s7pool_sup_native, start_link, []},
+%%            permanent, infinity, supervisor, [s7pool_sup_native]}
         ,
         {mqtt_pub_pool_sup,
             {mqtt_pub_pool_sup, start_link, []},
@@ -80,10 +80,10 @@ init([]) ->
             {s7reader_sup, start_link, []},
             permanent, infinity, supervisor, [s7reader_sup]}
         ,
-        {s7reader_sup_native,
-            {s7reader_sup_native, start_link, []},
-            permanent, infinity, supervisor, [s7reader_sup]}
-        ,
+%%        {s7reader_sup_native,
+%%            {s7reader_sup_native, start_link, []},
+%%            permanent, infinity, supervisor, [s7reader_sup]}
+%%        ,
         {initial_task_starter,
             {initial_task_starter, start_link, []},
             permanent, 5000, worker, []}
