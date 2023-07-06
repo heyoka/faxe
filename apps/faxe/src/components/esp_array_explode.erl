7044d9e6d18b1dacfae06f85a32b369b8f970327
heyoka <erdverbindung@gmx.at>
2023-07-06 11:18:22 +0200

    keep property for array_explode

--- apps/faxe/src/components/esp_array_explode.erl
+++ apps/faxe/src/components/esp_array_explode.erl
@@ -21,13 +21,15 @@
    node_id,
    fields,
    as,
-   offset = 0
+   offset = 0,
+   keep
 }).
 
 options() -> [
    {fields, string_list},
    {as, string_list, undefined},
-   {time_offset, duration, <<"1s">>}
+   {time_offset, duration, <<"1s">>},
+   {keep, string_list, undefined}
    ].
 
 
@@ -39,19 +41,21 @@ check_options() ->
 wants() -> point.
 emits() -> point.
 
-init(NodeId, _Ins, #{fields := Fields, as := As0, time_offset := Offset0}) ->
+init(NodeId, _Ins, #{fields := Fields, as := As0, time_offset := Offset0, keep := Keep}) ->
    Offset = faxe_time:duration_to_ms(Offset0),
    As = case As0 of undefined -> Fields; _ -> As0 end,
-   {ok, all, #state{fields = Fields, node_id = NodeId, as = As, offset = Offset}}.
+   {ok, all, #state{fields = Fields, node_id = NodeId, as = As, offset = Offset, keep = Keep}}.
 
 process(_Inport, #data_point{ts = TsStart} = Point, State = #state{fields = Fields, as = As, offset = Offset}) ->
    DataList0 = lists:zip(As, flowdata:fields(Point, Fields)),
    %% filter non existing fields
    [{_Alias, List}|_R] = DataList = [{A, DEntry} || {A, DEntry} <- DataList0, DEntry /= undefined],
    OutPort = 1,
+   %% list of values to keep for the current point
+   KeepList = keep_list(Point, State#state.keep),
    {_,InitPoints} = lists:foldl(
       fun(_, {TimeStamp, PList}) ->
-         {TimeStamp + Offset, PList ++ [{OutPort, #data_point{ts = TimeStamp}}]}
+         {TimeStamp + Offset, PList ++ [{OutPort, flowdata:set_fields(#data_point{ts = TimeStamp}, KeepList)}]}
       end, {TsStart, []}, List),
 
    Out = map_data(DataList, InitPoints),
@@ -67,6 +71,11 @@ map_data([{AsName, DList}|DataList], PointsAcc) ->
       end,
       lists:zip(DList, PointsAcc)),
    map_data(DataList, NewAcc).
+
+keep_list(_P, undefined) ->
+   [];
+keep_list(P, Fields) when is_list(Fields) ->
+   lists:zip(Fields, flowdata:fields(P, Fields)).
 
 
 
